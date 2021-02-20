bfb9761f6237c9cfb7c1df6a6a746f006fa219c7
Pedro Marins <phmarins@hotmail.com>
2021-02-20 13:40:24 -0300

    feat: adding deashboard style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,156 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { FiClock, FiPower } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+    Container,
+    Header,
+    HeaderContent,
+    Profile,
+    Content,
+    Schedule,
+    Calendar,
+    NextAppointment,
+    Section,
+    Appointment,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+const Dashboard: React.FC = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const { signOut, user } = useAuth();
+
+    const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+        if (modifiers.available) {
+            setSelectedDate(day);
+        }
+    }, []);
+
+    return (
+        <Container>
+            <Header>
+                <HeaderContent>
+                    <img src={logoImg} alt="GoBarber" />
+                    <Profile>
+                        <img src={user.avatar_url} alt={user.name} />
+                        <div>
+                        <span>Bem vindo,</span>
+                        <strong>{user.name}</strong>
+                        </div>
+                    </Profile>
+                    <button type="button" onClick={signOut}>
+                        <FiPower />
+                    </button>
+                </HeaderContent>
+            </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Horários agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 06</span>
+                        <span>Segunda-feira</span>
+                    </p>
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+                        <div>
+                            <img src="https://avatars.githubusercontent.com/u/25574889?s=460&u=2d951b7e1fd67bd3ae24b1c5c9a98811adbb3882&v=4" alt=""></img>
+                            <strong>Pedro Marins</strong>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                        </div>
+                    </NextAppointment>
+
+                    <Section>
+                        <strong>Manhã</strong>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img src="" alt=""/>
+                            </div>
+                        </Appointment>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img src="" alt=""/>
+                            </div>
+                        </Appointment>
+                    </Section>
+
+                    <Section>
+                        <strong>Tarde</strong>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img src="" alt=""/>
+                            </div>
+                        </Appointment>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                            <div>
+                                <img src="" alt=""/>
+                            </div>
+                        </Appointment>
+                    </Section>
+                </Schedule>
+                <Calendar>
+                    <DayPicker
+                        weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+                        fromMonth={new Date()}
+                        disabledDays={[
+                            { daysOfWeek: [0, 6] }
+                        ]}
+                        modifiers={{
+                            available: { daysOfWeek: [1, 2, 3, 4, 5] },
+                        }}
+                        selectedDays={selectedDate}
+                        onDayClick={handleDateChange}
+                        months={[
+                            'Janeiro',
+                            'Fevereiro',
+                            'Março',
+                            'Abril',
+                            'Maio',
+                            'Junho',
+                            'Julho',
+                            'Agosto',
+                            'Setembro',
+                            'Outubro',
+                            'Novembro',
+                            'Dezembro',
+                        ]}
+                    />
+                </Calendar>
+            </Content>
+        </Container>
+    );
+};
 
 export default Dashboard;
